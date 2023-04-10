7506154014f845fc4471b12ea2312a05f037da59
islammegdoude <islam.megdoude@gmail.com>
2023-04-10 23:39:57 +0100

    islam

--- Layout/passQuiz.php
+++ Layout/passQuiz.php
@@ -1,125 +1,81 @@
 <?php
+// Start session
 session_start();
-if (!isset($_SESSION['email'])) {
-    header('location:singIn.php');
-}
+//if (!isset($_SESSION['email'])) {
+//    header('location:singIn.php');
+//}
 
-// include '../includes/functions.php';
-// $quizs = getAllQuizs();
-$id_Q = $_GET['id'];
-$questions=getAllQuestion($id_Q);
+$idQ = $_GET['id'];
+include '../includes/functions.php';
+//$allInfo = getAllInfoQuizById($idQ);
+$titleQ = getTitleQuiz($idQ);
+$questions = getAllQuestion($idQ);
 ?>
 
 
-<!doctype html>
-<html lang="en">
 
+
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.1/assets/img/favicons/favicon.ico">
-
-    <title>Quizy</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/dashboard/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="../css/dashboard.css" rel="stylesheet">
+	<title>Quiz Participation Form</title>
+	<!-- Include Bootstrap CSS -->
+	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
 </head>
 
 <body>
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="../index.php">Quizy</a>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="../includes/logOut.php">Sign out</a>
-            </li>
-        </ul>
-    </nav>
+	<div class="container">
+		<h1>Quiz Participation Form</h1>
+		<form method="post" action="passquiz.php">
+			<h2><?php echo $titleQ; ?></h2>
+			<?php
 
-    <div class="container-fluid">
-        <div class="row">
-            <!-- sidebar -->
-            <?php include '../includes/sidebar.php'; ?>
-
-            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Passage Quiz</h1>
-                </div>
-                <!-- passage content -->
-                <div>
-
-                    <form>
-                        <fieldset disabled>
-                            <legend>Title quiz</legend>
-                            <?php
-                         
-                            foreach($questions as $index => $questions){                           
-                            ?>
-                            <div class="mb-3">
-                                <label></label>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        Default checkbox
-                                    </label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        Default checkbox
-                                    </label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        Default checkbox
-                                    </label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        Default checkbox
-                                    </label>
-                                </div>
-                            </div>
-                           
-                            <?php }?>
-                            
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </fieldset>
-                    </form>
-                </div>
-
-            </main>
-        </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>
-        window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')
-    </script>
-    <script src="../js/popper.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-
-    <!-- Icons -->
-    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-    <script>
-        feather.replace()
-    </script>
-
-    <!-- Graphs -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.1/dist/Chart.min.js"></script>
-
+			// Output each question with choices
+			foreach ($questions as $index => $question) {
+				?>
+				<div class="card">
+					<div class="card-header">Question <?php echo $index + 1; ?></div>
+					<div class="card-body">
+						<p><?php echo $question["title_question"]; ?></p>
+						<div class="form-group">
+							<div class="form-check">
+								<input class="form-check-input" type="radio" name="question_<?php echo $index; ?>" value="<?php echo $question["ch1"]; ?>">
+								<label class="form-check-label">
+									<?php echo $question["ch1"]; ?>
+								</label>
+							</div>
+							<div class="form-check">
+								<input class="form-check-input" type="radio" name="question_<?php echo $index; ?>" value="<?php echo $question["ch2"]; ?>">
+								<label class="form-check-label">
+									<?php echo $question["ch2"]; ?>
+								</label>
+							</div>
+							<div class="form-check">
+								<input class="form-check-input" type="radio" name="question_<?php echo $index; ?>" value="<?php echo $question["ch3"]; ?>">
+								<label class="form-check-label">
+									<?php echo $question["ch3"]; ?>
+								</label>
+							</div>
+							<div class="form-check">
+								<input class="form-check-input" type="radio" name="question_<?php echo $index; ?>" value="<?php echo $question["ch4"]; ?>">
+								<label class="form-check-label">
+									<?php echo $question["ch4"]; ?>
+								</label>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php
+			}
+			?>
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+		
+	</div>
+	<!-- Include Bootstrap JS -->
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
 </body>
-
 </html>
+
